fd5b2c420719145d41038bbfac493e1fc9759243
Thiha <thihakyawhtin.mm@gmail.com>
2022-02-22 22:41:09 +0630

    db context

---
+++ POS.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POS.Core.Aggregates;
+using POS.Core.Entities;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace POS.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            var entries = ChangeTracker.Entries<BaseEntity>()
+               .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+            foreach (var entityEntry in entries)
+            {
+                entityEntry.Entity.UpdatedOnUtc = DateTime.UtcNow;
+
+                if (entityEntry.State == EntityState.Added)
+                {
+                    entityEntry.Entity.Status = true;
+                    entityEntry.Entity.CreatedOnUtc = DateTime.UtcNow;
+                }
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        public override int SaveChanges()
+        {
+            return SaveChangesAsync().GetAwaiter().GetResult();
+        }
+    }
+}
