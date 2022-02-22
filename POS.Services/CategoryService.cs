8c42a22e782f55578ff40a2d5ea3c86de2a781c7
Thiha <thihakyawhtin.mm@gmail.com>
2022-02-22 22:41:49 +0630

    category services

---
+++ POS.Services/CategoryService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POS.Core.Entities;
+using POS.Infrastructure.Data;
+using POS.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace POS.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IRepository<Category> _categoryRepo;
+
+        public CategoryService(IRepository<Category> categoryRepo)
+        {
+            _categoryRepo = categoryRepo;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+        {
+            List<Category> categories = await _categoryRepo.GetAll().ToListAsync();
+            return categories;
+        }
+
+        public async Task<Category> GetCategoryByIdAsync(long categoryId)
+        {
+            Category category = await _categoryRepo.GetByIdAsync(categoryId);
+            return category;
+        }
+
+        public async Task InsertCategoryAsync(Category category)
+        {
+            await _categoryRepo.InsertAsync(category);
+        }
+
+        public async Task UpdateCategoryAsync(Category category)
+        {
+            await _categoryRepo.UpdateAsync(category);
+        }
+
+        public async Task DeleteCategoryAsync(Category category)
+        {
+            await _categoryRepo.DeleteAsync(category);
+        }
+    }
+}
---
+++ POS.Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using POS.Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace POS.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> GetAllCategoriesAsync();
+
+        Task<Category> GetCategoryByIdAsync(long categoryId);
+
+        Task InsertCategoryAsync(Category category);
+
+        Task UpdateCategoryAsync(Category category);
+
+        Task DeleteCategoryAsync(Category category);
+    }
+}
