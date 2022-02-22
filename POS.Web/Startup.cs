34b51d19c046fc67ec12301791106367ee202464
Thiha <thihakyawhtin.mm@gmail.com>
2022-02-22 22:44:25 +0630

    setup db context and initial migration

--- POS.Web/Startup.cs
+++ POS.Web/Startup.cs
@@ -1,12 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using POS.Infrastructure.Data;
+using POS.Services;
+using POS.Services.Interfaces;
 
 namespace POS.Web
 {
@@ -22,6 +22,13 @@ namespace POS.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                 builder => builder.MigrationsAssembly("POS.Infrastructure")));
+
+            services.AddTransient<ICategoryService, CategoryService>();
+            services.AddTransient(typeof(IRepository<>), typeof(EfRepository<>));
+
             services.AddControllersWithViews();
         }
 
