af5f34331a808949f85b8380daa91a0e47df24eb
Thiha <thihakyawhtin.mm@gmail.com>
2022-02-22 22:41:21 +0630

    generic repository pattern

---
+++ POS.Infrastructure/Data/EfRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POS.Core.Aggregates;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POS.Infrastructure.Data
+{
+    public class EfRepository<T> : IRepository<T> where T : class, IAggregateRoot
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public EfRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<T> GetByIdAsync(long id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbContext.Set<T>().AsQueryable();
+        }
+
+        public async Task InsertAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            return _dbContext.SaveChangesAsync();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            return _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ POS.Infrastructure/Data/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using POS.Core.Aggregates;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POS.Infrastructure.Data
+{
+    public interface IRepository<T> where T : class, IAggregateRoot
+    {
+        Task<T> GetByIdAsync(long id);
+
+        IQueryable<T> GetAll();
+
+        Task InsertAsync(T entity);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(T entity);
+    }
+}
