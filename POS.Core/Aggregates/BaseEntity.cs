33be76df765583b693aa178512aea883c67c7d28
Thiha <thihakyawhtin.mm@gmail.com>
2022-02-22 22:40:23 +0630

    core base aggregates

---
+++ POS.Core/Aggregates/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace POS.Core.Aggregates
+{
+    /// <summary>
+    /// Base class for entities
+    /// </summary>
+    public abstract class BaseEntity
+    {
+        public long Id { get; set; }
+
+        public DateTime CreatedOnUtc { get; set; }
+
+        public DateTime UpdatedOnUtc { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
