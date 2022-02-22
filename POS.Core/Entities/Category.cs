e00d9699bb4ad2e8390332c5eddc594e1c0f50ba
Thiha <thihakyawhtin.mm@gmail.com>
2022-02-22 22:40:39 +0630

    category entity

---
+++ POS.Core/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using POS.Core.Aggregates;
+using System;
+
+namespace POS.Core.Entities
+{
+    public class Category : BaseEntity, IAggregateRoot
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
